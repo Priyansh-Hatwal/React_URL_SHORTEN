34c06be500f0e05af85d86bd8e92a66e8abdd42a
Priyansh-Hatwal <104242408+Priyansh-Hatwal@users.noreply.github.com>
2024-09-13 02:28:44 +0530

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -18,7 +18,7 @@ const Form = () => {
         event.preventDefault();
         const link = formData.link;
         try {
-            const out = await axios.get(`https://www.shareaholic.com/v2/share/shorten_link?apikey=8943b7fd64cd8b1770ff5affa9a9437b&url=${link}`);
+            const out = await axios.get(`https://www.shareaholic.com/v2/share/shorten_link?apikey=c134ffd7a009ee909879406053c80122&url=${link}`);
             console.log("Output is ",out)
             const ans = out.data.data;
             setFormData(prev => ({
